7443f46a7e6d355720fc045b539e34b9f2c84ebc
Kai <u7moench@gmail.com>
2020-04-10 15:27:41 +0200

    Make room for codeGenAssembly, use command line parsing

---
+++ src/compiler-backend/CodeGenAssembly.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <BinaryExpr.h>
+#include <UnaryExpr.h>
+#include <LiteralExpr.h>
+
+namespace aeeh {
+namespace code_gen {
+
+	inline bool generateAssembly(ExprPtr expr) {
+		std::cout << "Here we must generate the assembly.\n";
+	}
+}
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,54 @@
 #include <iostream>
-
 #include <Chunk.h>
 #include <Debug.h>
 #include "VirtualMachine.h"
 #include <Lexer.h>
 #include <Parser.h>
 #include <CodeGen.h>
+#include <CodeGenAssembly.h>
 #include <BinaryExpr.h>
 #include <LiteralExpr.h>
 #include <NotSoPrettyPrinter.h>
+#include <include/cxxopts.hpp>
 
-int main() {
-    std::cout << "Lexer:" << std::endl;
-    Lexer lexer("-2");
+namespace cg = aeeh::code_gen;
+
+int main(int argc, char** argv) 
+{
+	// TODO move command line parsing to method.
+	cxxopts::Options options("test", "A brief description");
+	
+	options.add_options()
+		("a,assembly", "Output assembly instead of using the vm",
+		 cxxopts::value<bool>()->default_value("true")
+		)
+	;
+	auto result = options.parse(argc, argv);
+
+	auto outputAssembly = result["assembly"].as<bool>();
+
+	// TODO make this functional -> why does this need an object ???
+    Lexer lexer("-2 + 23");
     auto tokens = lexer.lex();
-    for (auto t : *tokens) {
-        std::cout << " " << t.toString();
-    }
-    std::cout << std::endl;
-    std::cout << std::endl;
 
+	// TODO make this functional -> why does this need an object ???
     Parser parser(tokens);
     auto ast = parser.parse();
 
     NotSoPrettyPrinter printer;
     std::cout << "AST:" << std::endl << printer.print(ast) << std::endl;
 
-    CodeGen codeGen;
-    auto chunk = codeGen.generate(ast);
+	if (outputAssembly) {
+		auto assembly = cg::generateAssembly(ast);
+	} else {
+        CodeGen codeGen;
+        auto chunk = codeGen.generate(ast);
 
-    dissassembleChunk(chunk,"test chunk");
+        dissassembleChunk(chunk,"test chunk");
 
-    VirtualMachine vm;
-    vm.interpret(chunk);
+        VirtualMachine vm;
+        vm.interpret(chunk);
+	}
 
     return 0;
 }
