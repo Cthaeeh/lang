ebec8e005eb4d7945cd563db1bd4a15f5a5e993b
Kai <u7moench@gmail.com>
2020-04-19 22:26:44 +0200

    Clean up Lexer

--- src/compiler-frontend/Lexer.cpp
+++ src/compiler-frontend/Lexer.cpp
@@ -4,44 +4,76 @@
 #include "Lexer.h"
 #include "Token.h"
 #include <functional>
+#include <iostream>
 #include <map>
 #include <optional>
 #include <string>
+#include <string_view>
+#include <unordered_map>
 #include <vector>
 
 namespace aeeh {
 namespace frontend {
 namespace detail {
 
-struct StringView {
-  StringView(std::string::const_iterator begin,
-             std::string::const_iterator end)
-    :begin(begin),
-     end(end){}
+auto scanDepending(CharPredicate predicate, ScanFunction ifTrue, ScanFunction ifFalse)
+    -> ScanFunction {
 
-  const std::string::const_iterator begin;
-  const std::string::const_iterator end;
-};
+  return [predicate, ifTrue, ifFalse](std::string_view code) {
+    return (predicate(peek(code)))
+          ? ifTrue(code)
+          : ifFalse(code);
+  };
+}
 
-// One way to make this mostly functional is to introduce this Idea of a ScanProgess.
-// It is an optional token (maybe we were not able to scan anything) and a thing pointing
-// to the end of the token. 
-using ScanProgress = std::pair<std::optional<ast::Token>, std::string::const_iterator>; 
+// clang-format off
 
-// Now we can say what a ScanFunction is. StringView -> ScanProgress
-using ScanFunction = std::function<ScanProgress(const StringView &)>;
+static const auto dispatchTable = std::vector<std::pair<CharPredicate, ScanFunction>>
+ {
+  {isDigit, scanNumber},
+  {isAlpha, scanIdentifier},
+  {isChar('('), scanSingleCharacterToken(ast::TokenType::LEFT_PAREN)},
+  {isChar(')'), scanSingleCharacterToken(ast::TokenType::RIGHT_PAREN)},
+  {isChar('{'), scanSingleCharacterToken(ast::TokenType::LEFT_BRACE)},
+  {isChar('}'), scanSingleCharacterToken(ast::TokenType::RIGHT_BRACE)},
+  {isChar(','), scanSingleCharacterToken(ast::TokenType::COMMA)},
+  {isChar('.'), scanSingleCharacterToken(ast::TokenType::DOT)},
+  {isChar('-'), scanSingleCharacterToken(ast::TokenType::MINUS)},
+  {isChar('+'), scanSingleCharacterToken(ast::TokenType::PLUS)},
+  {isChar(';'), scanSingleCharacterToken(ast::TokenType::SEMICOLON)},
+  {isChar('*'), scanSingleCharacterToken(ast::TokenType::STAR)},
+  {isChar('/'), scanSingleCharacterToken(ast::TokenType::SLASH)},
+  {isChar(' '), skipChar},
+  {isChar('\n'),skipChar}, // TODO include line info again
+  {isChar('!'), scanDepending(isChar('='),
+                              scanDoubleCharacterToken(ast::TokenType::BANG_EQUAL),
+                              scanSingleCharacterToken(ast::TokenType::BANG))},
+  {isChar('='), scanDepending(isChar('='),
+                              scanDoubleCharacterToken(ast::TokenType::EQUAL_EQUAL),
+                              scanSingleCharacterToken(ast::TokenType::EQUAL))},
+  {isChar('<'), scanDepending(isChar('='),
+                              scanDoubleCharacterToken(ast::TokenType::LESS_EQUAL),
+                              scanSingleCharacterToken(ast::TokenType::LESS))},
+  {isChar('>'), scanDepending(isChar('='),
+                              scanDoubleCharacterToken(ast::TokenType::GREATER_EQUAL),
+                              scanSingleCharacterToken(ast::TokenType::GREATER))}
+  };
 
-// This is abit hacky I think, but since we dont really have chaining optionals it makes 
-// the code more readable
-using CharPredicate = std::function<bool(std::optional<char>)>;
+static const std::unordered_map<std::string, ast::TokenType> keywords_ = {
+  {"if",      ast::TokenType::IF},       
+  {"else",    ast::TokenType::ELSE},
+  {"false",   ast::TokenType::FALSE}, 
+  {"true",    ast::TokenType::TRUE},
+  {"for",     ast::TokenType::FOR},     
+  {"return",  ast::TokenType::RETURN},
+  {"while",   ast::TokenType::WHILE}};
 
+// clang-format on
 
 auto isDigit(std::optional<char> c) -> bool { return c >= '0' && c <= '9'; }
 
 auto isChar(char toCheck) -> CharPredicate {
-  return [toCheck](std::optional<char> c) {
-    return c == toCheck;
-  };
+  return [toCheck](std::optional<char> c) { return c == toCheck; };
 }
 
 auto isAlpha(std::optional<char> c) -> bool {
@@ -49,147 +81,109 @@ auto isAlpha(std::optional<char> c) -> bool {
 }
 
 /// Look at the second character of the view (If it is there)
-auto peek(const StringView &code) -> std::optional<char>{
-  auto it = code.begin;
-  it++;
-  if (it != code.end)
-    return *it;
-  else
+auto peek(std::string_view code) -> std::optional<char> {
+  if (code.size() < 2) {
     return std::nullopt;
+  }
+  return code[1];
 }
 
 /// Advance as long as digits are hit.
-auto scanNumber(const StringView &code) -> ScanProgress {
-  if (code.begin == code.end) {
-    return {std::nullopt, code.begin};
+auto scanNumber(std::string_view code) -> ScanProgress {
+  if (code.empty()) {
+    return {std::nullopt, code.begin()};
   }
 
-  auto it = code.begin;
-  while (isDigit(*it) && it != code.end) {
-    it++;
-  }
-  auto text = std::string(code.begin, it);
+  auto it = std::find_if(code.begin(), code.end(), std::not_fn(isDigit));
+
+  auto text = std::string(code.begin(), it);
   // TODO acually use the value ?
-  return std::make_pair(ast::Token(ast::TokenType::NUMBER, text, 0), it);
+  return {ast::Token(ast::TokenType::NUMBER, text, 0), it};
 }
 
 /// Advance as long as Letters are hit
-auto scanIdentifier(const StringView &code) -> ScanProgress {
-  if (code.begin == code.end) {
-    return {std::nullopt, code.begin};
+auto scanIdentifier(std::string_view code) -> ScanProgress {
+  if (code.empty()) {
+    return {std::nullopt, code.begin()};
   }
 
-  auto it = code.begin;
-  while (isAlpha(*it) && it != code.end) {
-    it++;
-  }
+  auto it = std::find_if(code.begin(), code.end(), std::not_fn(isAlpha));
 
-  auto text = std::string(code.begin, it);
+  auto text = std::string(code.begin(), it);
 
-  static const std::map<std::string, ast::TokenType> keywords_ = {
-    {"if", ast::TokenType::IF},       {"else", ast::TokenType::ELSE},
-    {"false", ast::TokenType::FALSE}, {"true", ast::TokenType::TRUE},
-    {"for", ast::TokenType::FOR},     {"return", ast::TokenType::RETURN},
-    {"while", ast::TokenType::WHILE}};
-
-  // TODO use optional map acess.
   if (keywords_.find(text) != keywords_.end()) {
-    return std::make_pair(ast::Token(keywords_.at(text), text, 0), it);
+    return {ast::Token(keywords_.at(text), text, 0), it};
   } else {
-    return std::make_pair(ast::Token(ast::TokenType::IDENTIFIER, text, 0), it);
+    return {ast::Token(ast::TokenType::IDENTIFIER, text, 0), it};
   }
 }
 
 auto scanSingleCharacterToken(ast::TokenType type) -> ScanFunction {
-  return [type](const StringView &code) {
-    auto text = std::string(code.begin, code.begin + 1);
-    return std::make_pair(ast::Token(type, text, 0),
-                          code.begin + 1); // TODO get line info back
+  return [type](std::string_view code) -> ScanProgress {
+    auto text = std::string(code.begin(), code.begin() + 1);
+    return {ast::Token(type, text, 0), code.begin() + 1};
+    // TODO get line info back
   };
 }
 
-auto scanDoubleCharacterToken(ast::TokenType type) -> ScanFunction  {
-  return [type](const StringView &code) {
-    auto text = std::string(code.begin, code.begin + 2);
-    return std::make_pair(ast::Token(type, text, 0),
-                          code.begin + 2); // TODO get line info back
+auto scanDoubleCharacterToken(ast::TokenType type) -> ScanFunction {
+  return [type](std::string_view code) -> ScanProgress {
+    auto text = std::string(code.begin(), code.begin() + 2);
+    return {ast::Token(type, text, 0),
+            code.begin() + 2}; // TODO get line info back
   };
 }
 
-auto skipChar(const StringView &code) {
-  return std::make_pair(std::nullopt, code.begin + 1);
+auto inline skipChar(std::string_view code) -> ScanProgress {
+  return {std::nullopt, code.begin() + 1};
 }
 
-auto scanToken(const StringView &code) -> ScanProgress 
-{
+auto scanToken(std::string_view code) -> ScanProgress {
 
-  // TODO I am sure this can be written more nicely.
-  if (code.begin == code.end) {
-    return {std::nullopt, code.begin};
+  if (code.empty()) {
+    return {std::nullopt, code.begin()};
   }
 
-  auto c = *code.begin;
+  auto c = *code.begin();
 
-  static const auto myMap = std::vector<std::pair<CharPredicate, ScanFunction>>
-
-  {{isChar('('),scanSingleCharacterToken(ast::TokenType::LEFT_PAREN)},
-   {isChar(')'),scanSingleCharacterToken(ast::TokenType::RIGHT_PAREN)},
-   {isChar('{'),scanSingleCharacterToken(ast::TokenType::LEFT_BRACE)},
-   {isChar('}'),scanSingleCharacterToken(ast::TokenType::RIGHT_BRACE)},
-   {isChar(','),scanSingleCharacterToken(ast::TokenType::COMMA)},
-   {isChar('.'),scanSingleCharacterToken(ast::TokenType::DOT)},
-   {isChar('-'),scanSingleCharacterToken(ast::TokenType::MINUS)},
-   {isChar('+'),scanSingleCharacterToken(ast::TokenType::PLUS)},
-   {isChar(';'),scanSingleCharacterToken(ast::TokenType::SEMICOLON)},
-   {isChar('*'),scanSingleCharacterToken(ast::TokenType::STAR)},
-   {isChar('/'),scanSingleCharacterToken(ast::TokenType::SLASH)},
-   {isChar(' '),skipChar},
-   {isChar('\n'),skipChar},     // TODO include line infor again
-
-   {isChar('!'), [](const StringView &code) {return ((isChar('=')(peek(code))) ? scanDoubleCharacterToken(ast::TokenType::BANG_EQUAL)(code) : scanSingleCharacterToken(ast::TokenType::BANG)(code));}}, 
-   
-   {isChar('='), [](const StringView &code) {return  ((isChar('=')(peek(code))) ? scanDoubleCharacterToken(ast::TokenType::EQUAL_EQUAL)(code) : scanSingleCharacterToken(ast::TokenType::EQUAL)(code));}}, 
-   
-   {isChar('<'), [](const StringView &code) {return  ((isChar('=')(peek(code))) ? scanDoubleCharacterToken(ast::TokenType::LESS_EQUAL)(code) : scanSingleCharacterToken(ast::TokenType::LESS)(code));}}, 
-   
-   {isChar('>'), [](const StringView &code) {return  ((isChar('=')(peek(code))) ? scanDoubleCharacterToken(ast::TokenType::GREATER_EQUAL)(code) : scanSingleCharacterToken(ast::TokenType::GREATER)(code));}},
-
-   {isDigit, scanNumber},
-   {isAlpha, scanIdentifier}};
-
-  for (const auto &[charPredicate, scanFunction] : myMap) {
+  for (const auto &[charPredicate, scanFunction] : dispatchTable) {
     if (charPredicate(c))
       return scanFunction(code);
   }
-  // TODO somehow propagate here that no progress was made, because no character matched.
-  return {std::nullopt,code.begin};
+
+  std::cout << "No progress made. -> PROBLEM" << std::endl;
+  // TODO somehow propagate here that no progress was made, because no character
+  // matched.
+  return {std::nullopt, code.begin()};
 }
 
-std::vector<ast::Token> lex(const std::string &source) {
-  auto tokens = std::vector<ast::Token>();
-
-  if (source.empty()) {
-    return tokens;
+std::vector<ast::Token> lex(std::string_view code) {
+  if (code.empty()) {
+    return {};
   }
 
-  auto pos = source.begin();
-  while (pos != source.end()) {
-    auto [token, newPos] = scanToken(StringView(pos, source.end()));
+  auto tokens = std::vector<ast::Token>();
 
-    if (token.has_value()) {
-      tokens.push_back(token.value());
-    } 
+  auto currentPos = code.begin();
+  while (currentPos != code.end()) {
 
-    pos = newPos;
+    // 'Hack' to transform pair of iterators too string_view.
+    // Not in the standard for some reason you can look up online.
+    auto scanProgress = scanToken(
+        std::string_view(&*currentPos, std::distance(currentPos, code.end())));
+
+    if (scanProgress.token.has_value()) {
+      tokens.push_back(scanProgress.token.value());
+    }
+
+    currentPos = scanProgress.endOfToken;
   }
   return tokens;
 }
 
 } // namespace detail
 
-std::vector<ast::Token> lex(const std::string &source) {
-  return detail::lex(source);
-}
+std::vector<ast::Token> lex(std::string_view code) { return detail::lex(code); }
 
 } // namespace frontend
 } // namespace aeeh
--- src/compiler-frontend/Lexer.h
+++ src/compiler-frontend/Lexer.h
@@ -8,16 +8,53 @@
 #include <string>
 #include <vector>
 #include <Token.h>  
+#include <optional>
+#include <functional>
 
-/**
- * Ultra basic lexer
- * TODO unicode support
- * TODO support literals
- */
 namespace aeeh{
 namespace frontend{
+namespace detail{
 
-  std::vector<ast::Token> lex(const std::string &code);
+// One way to make this mostly functional is to introduce this Idea of a
+// ScanProgess. It is an optional token (maybe we were not able to scan
+// anything) and a thing pointing to the end of the token.
+struct ScanProgress {
+  std::optional<ast::Token> token;
+  std::string_view::const_iterator endOfToken;
+};
+
+// Now we can say what a ScanFunction is. StringView -> ScanProgress
+using ScanFunction = std::function<ScanProgress(std::string_view)>;
+
+// This is abit hacky I think, but since we dont really have chaining optionals
+// it makes the code more readable
+using CharPredicate = std::function<bool(std::optional<char>)>;
+
+auto isDigit(std::optional<char> c) -> bool;
+
+auto isChar(char toCheck) -> CharPredicate; 
+
+auto isAlpha(std::optional<char> c) -> bool; 
+
+auto peek(std::string_view code) -> std::optional<char>;
+
+auto scanNumber(std::string_view code) -> ScanProgress;
+
+auto scanIdentifier(std::string_view code) -> ScanProgress;
+
+auto scanSingleCharacterToken(ast::TokenType type) -> ScanFunction;
+
+auto scanDoubleCharacterToken(ast::TokenType type) -> ScanFunction;
+
+auto inline skipChar(std::string_view code) -> ScanProgress;
+
+auto scanToken(std::string_view code) -> ScanProgress;
+
+auto lex(std::string_view code) -> std::vector<ast::Token>;
+
+}
+
+auto lex(std::string_view code) -> std::vector<ast::Token>;
 
 }
 }
